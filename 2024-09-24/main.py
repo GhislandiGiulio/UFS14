94fc7eaad893c60f824b3b4476bdc3c536e62da5
GhislandiGiulio <giulio.ghislandi@itsrizzoli.it>
2024-10-18 15:33:18 +0000

    updated files end of lecture

--- 2024-09-24/main.py
+++ 2024-09-24/main.py
@@ -2,9 +2,19 @@ import os
 
 model_dir = os.environ['SM_MODEL_DIR']
 
-with open(model_dir + '/output.txt', 'w') as f:
-    f.write('Ciao')
+with open(model_dir + '/output_model.txt', 'w') as f:
+    f.write('Ciao, sono il modello di ML!')
     
 output_dir = os.environ['SM_OUTPUT_DIR']
-with open(output_dir + '/output.txt', 'w') as f:
-    f.write('Ciao')
+with open(output_dir + '/output_data.txt', 'w') as f:
+    f.write('Ciao, sono i LOG del training!')
+    
+input_dir = os.environ['SM_INPUT_DIR']    
+
+lines = 0
+with open(input_dir + "/data/training/train.csv", 'r') as f:
+    lines = len(f.readlines())
+    print("#######Total number of lines:", lines)
+          
+with open(output_dir + "/output_lines.txt", 'w') as f:
+    f.write(f"Ciao, le righe sono {lines}")
