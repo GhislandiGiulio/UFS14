305f3b35b428f75a357d16fd09e0daaa995fad2f
GhislandiGiulio <giulio.ghislandi@itsrizzoli.it>
2024-11-22 16:40:23 +0000

    endpoint funzionante

--- inference/inference.py
+++ inference/inference.py
@@ -11,25 +11,35 @@ from flask import request
 logging.debug('Init a Flask app')
 app = Flask(__name__)
 
-model_dir = os.environ['SM_MODEL_DIR']
-model = models.load_model(f"{model_dir}/abalone_model.keras")
+model_dir = "/model"
+
+#model = models.load_model(f"{model_dir}/abalone_model.keras")
 
 def doit(lunghezza, diametro):
-    predict_input = np.array([
-        [lunghezza,diametro,0.125,0.5095,0.2165,0.1125,0.165,9]
-    ])
-    predict_result = model.predict(predict_input)
+    #predict_input = np.array([
+    #    [lunghezza,diametro,0.125,0.5095,0.2165,0.1125,0.165,9]
+    #])
+    #predict_result = model.predict(predict_input)
+    
+    #test = model.input_shape
 
     return json.dumps({
         "inputs": predict_input.tolist(),
         "predict_result": predict_result.tolist()
     })
 
-@app.route('/ping')
+@app.route('/invocations', methods=['POST'])
+def invocations():
+    data = request.get_json()  # Parse the incoming JSON payload
+    lunghezza = data['lunghezza']
+    diametro = data['diametro']
+    return doit(lunghezza, diametro)
+    
+@app.route('/ping', methods=['GET'])
 def ping():
-    logging.debug('Hello from route /ping')
-    lunghezza = request.args.get('lunghezza')
-    diametro = request.args.get('diametro')
+    try:
+        status = 200  # Healthy
+    except:
+        status = 503  # Service Unavailable
 
-    return doit(float(lunghezza), float(diametro))
-
+    return '', status
